3083e5f4009aeca0532af9369cdfa9ffb5e2f946
fdym <fdym_dlygzh@163.com>
2024-11-18 20:13:37 +0800

    技术测试24/11/18/b：通过lambda表达式简化代码。Technical test 24/11/18/b: Simplify code through lambda expressions.

--- server_docs/urls.py
+++ server_docs/urls.py
@@ -2,10 +2,7 @@ from django.urls import path
 from django.conf import settings
 from django.shortcuts import render
 
-#def yourfile(request):return render(request, 'yourfile.html')
-def test(request):return render(request, 'test.html')
-
 urlpatterns = [
-    #path('yourfile', yourfile, 'yourfile'),
-    path('test', test, name='test'),
+    #path('yourfile', lambda a:render(a, 'yourfile.html'), 'yourfile'),
+    path('test', lambda a:render(a, 'test.html'), name='test'),
 ]
